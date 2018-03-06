20fd272c707f65c300388507bc7483e6e40ac02d
Steven Webb <cy1701@gmail.com>
2018-03-05 20:44:11 -0500

    Sample Secrets.h file

---
+++ ESP_MQTT_Digital_LEDs/secrets.h
@@ -0,0 +1,7 @@
+const char* Secretssid = "YourSSID"; //type your WIFI information inside the quotes
+const char* SecretWifipassword = "YourWIFIpassword";
+const char* Secretmqtt_server = "SECRET_SOMETHING";
+const char* Secretmqtt_username = "yourMQTTusername";
+const char* Secretmqtt_password = "yourMQTTpassword";
+const int Secretmqtt_port = 1883;
+
